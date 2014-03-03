7dfe90d02aaa01df65ea94d0adee385eea6cff87
Kapa <kapa@sipaksi.net>
2014-03-03 16:58:12 +0100

    Added documentation.

---
+++ jsdoc.conf.js
@@ -0,0 +1,17 @@
+{
+    "tags": {
+        "allowUnknownTags": true
+    },
+    "source": {
+        "include": ["./Library.js"],
+        "includePattern": ".+\\.js(doc)?$"
+    },
+    "plugins": [],
+    "templates": {
+        "cleverLinks": true,
+        "monospaceLinks": false
+    },
+    "opts": {
+        "destination": "./docs/"
+    }
+}
